a8666ad64fb320e7366e4f51e045bc51b3a8b2e6
Vinicius Meirelles <vinicius_meirelless@hotmail.com>
2023-04-10 13:58:28 -0300

    Placing pieces on the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -41,7 +41,8 @@ public class Board {
 	}
 	
 	public void placePiece(Piece piece, Position position) {
-		
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position;
 	}
 	
 	public Piece removePiece(Position position) {
